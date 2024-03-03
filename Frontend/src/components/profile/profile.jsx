295878086c9967cd4de7f2e9fe9f0313703f69a3
Jayanth Karnati <147145667+Jayanth-137@users.noreply.github.com>
2024-03-03 22:19:02 +0530

    Update profile.jsx

--- Frontend/src/components/profile/profile.jsx
+++ Frontend/src/components/profile/profile.jsx
@@ -18,8 +18,8 @@ function Profile(){
         </div>
         <h1 className="profile_name">{user.name}</h1>
         <p className="profile_email">{user.email}</p>
-        <p className="profile_phone">9827827842</p>
-        <p className="profile_gender">Male</p>
+        <p className="profile_phone">Mobile Number</p>
+        <p className="profile_gender">Gender</p>
         <button type="submit" className="profile_button">Update Profile</button>
     </div>
     </div>
