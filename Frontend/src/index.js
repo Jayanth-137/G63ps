3b7e54f3d84d1a628d68c3d8f2735631a8e516d5
Jayanth-137 <jayanth.karnati2003@gmail.com>
2024-03-05 12:42:18 +0530

    modified:   Frontend/package-lock.json 	modified:   Frontend/package.json 	modified:   Frontend/src/index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = document.querySelector("#root");
 const rootInstance = createRoot(root);
@@ -10,6 +12,8 @@ const rootInstance = createRoot(root);
   rootInstance.render(
     <GoogleOAuthProvider clientId="832124440981-lmfh70fjm7g5c0oldrm10049mj3o7qep.apps.googleusercontent.com">
       <App/>
+      <Analytics/>
+      <SpeedInsights/>
     </GoogleOAuthProvider>
   );
 
